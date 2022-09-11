556db0969b98a6fbd0596acd27d415618baac4a8
peanuts07 <keirapienaar@icloud.com>
2022-09-11 19:38:04 +0200

    made changes / filled in the code

--- POE PART ONE/Hero.cs
+++ POE PART ONE/Hero.cs
@@ -10,8 +10,43 @@ namespace POE_PART_ONE
     {
         public Hero(char x, char y, int HP) : base(x, y)
         {
-            //set the hp and max hp of hero based on parameter
-            //hero's damage is always 2
+            this.HP = 10;
+            this.maxHP = maxHP;
+            this.damage = 2;
+            this.x = x;
+            this.y = y;
+        }
+
+        public override int ReturnMove(int num)
+        {
+
+            // a loop to check the vision array againt the move given .. how would we check valid movement ? 
+            // so if the . = . then movemnt is vaild 
+            // if the . = sth else then its no movement , the value retuened from enum is 0 
+            // a number from the enum must then be used in main code to move the char in the dicrection
+
+            //if(num == 0) // 
+            //{
+            //    if (this.damage > 0)
+            //    {
+
+            //    }
+            //}
+            //else if ()
+            //{
+
+            //}
+
+            return num;
+            //randomizes a direction
+        }
+        public override String ToString()
+        {
+            damage.ToString();
+            x.ToString();
+            y.ToString();
+            //This is defined in the Character subclasses and overrides the traditional Object ToString() method.
+            return " Hero at: " + x + y + damage;
         }
     }
 }
--- POE PART ONE/SwampCreature.cs
+++ POE PART ONE/SwampCreature.cs
@@ -8,9 +8,35 @@ namespace POE_PART_ONE
 {
     class SwampCreature : Enemy
     {
-        protected SwampCreature(char x, char y) : base(x, y)//the constructor that receives x and y positions
+        private string name = "kes";
+        public string Name
         {
-
+            get { return name; } // get method 
+            set { name = value; }
         }
+        public SwampCreature(int damage, int startingHP, int maxHP, char x, char y) : base(damage, startingHP, maxHP, x, y)  //x and y positions)  /// constructor initializer??
+        {
+            this.maxHP = 10;
+            this.damage = 1;
+            this.x = x; // delagting back to the other class??
+            this.y = y;
+        }
+
+        public override int ReturnMove(int num)
+        {
+            num = rnd.Next();
+            if (num == 0)
+            {
+                // if position of enemy is not = obsticle 
+            }
+            else
+            { // reroll
+                num = rnd.Next();
+
+            }
+            return num;
+            //randomizes a direction
+        }
+
     }
 }
