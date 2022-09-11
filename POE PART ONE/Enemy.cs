274312519361b2192b55913e9d3af9ad2bb56395
peanuts07 <keirapienaar@icloud.com>
2022-09-11 14:25:26 +0200

    created constructors for enemy class

--- POE PART ONE/Enemy.cs
+++ POE PART ONE/Enemy.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace POE_PART_ONE
 {
-    internal class Enemy
+    abstract class Enemy : Character
     {
+        public Random rnd = new Random();
+        public int num;
+
+        public Enemy(char x, char y) : base(x, y)
+        {
+
+        }
+
+        public Enemy(int damage, int startingHP, int maxHP, char x, char y) : base(x, y)  //x and y positions)
+        {
+
+        }
     }
 }
