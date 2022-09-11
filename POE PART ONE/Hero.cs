2953462cfc65b87130cea282d92f576c723630c7
peanuts07 <keirapienaar@icloud.com>
2022-09-11 14:22:00 +0200

    Created a Hero class with constructor

---
+++ POE PART ONE/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PART_ONE
+{
+    class Hero : Character
+    {
+        public Hero(char x, char y, int HP) : base(x, y)
+        {
+            //set the hp and max hp of hero based on parameter
+            //hero's damage is always 2
+        }
+    }
+}
