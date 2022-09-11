c44093e55f43eaf97809b9e5c7497736e3d2b574
peanuts07 <keirapienaar@icloud.com>
2022-09-11 19:32:23 +0200

    Declared variables

--- POE PART ONE/Tile.cs
+++ POE PART ONE/Tile.cs
@@ -8,9 +8,24 @@ namespace POE_PART_ONE
 {
     public abstract class Tile
     {
-        public Tile(int X, int Y) //Constructor that recieves initial values for these variables
+        //variables
+        protected char x; //FIGURE OUT HOW TO DO BOTH PROTECTED AND PUBLIC
+        protected char y;
+        public enum TileType
+        {
+            Hero, // 0
+            Enemy, // 1
+            Gold, // 2
+            Weapon // 3
+        }
+        public Tile()
         {
 
+        }
+        public Tile(int X, int Y) //Constructor that recieves initial values for these variables
+        {
+            this.x = x; // only in this class 
+            this.y = y;
         }
     }
 }
