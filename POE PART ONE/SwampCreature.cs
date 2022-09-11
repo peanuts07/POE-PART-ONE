d5a3f49919a5ea8cc3d75169dee4bac3ad40f4d6
peanuts07 <keirapienaar@icloud.com>
2022-09-11 14:20:58 +0200

    Created Swamp creature cclass with constructor

---
+++ POE PART ONE/SwampCreature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PART_ONE
+{
+    class SwampCreature : Enemy
+    {
+        protected SwampCreature(char x, char y) : base(x, y)//the constructor that receives x and y positions
+        {
+
+        }
+    }
+}
